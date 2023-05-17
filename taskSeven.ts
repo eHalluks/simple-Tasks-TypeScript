5eeb8fb6c64b777c9f72a2af0087e1165839525f
eHalluks <mateo.kaczynski@gmail.com>
2023-05-17 23:34:33 +0200

    next task has been completed successfully.

--- taskSeven.ts
+++ taskSeven.ts
@@ -1,3 +1,17 @@
+/*
+
+    TASK: 7 (#fundamentals)
+
+    DESCRIPTION: In Typescript exist three data modifiers
+    ['public', 'private', 'protected']
+
+    [ done ]TODO: 1# Create a simple example with PUBLIC
+    [ done ]TODO: 2# Create a simple example with Private
+    [  ]TODO: 3# Create a simple example with Protected
+
+ */
+
+
 import chalk from 'chalk';
 
 const createdObject: (Superset | ProgrammingLanguage)[] = [];
@@ -53,3 +67,68 @@ const displayObject = () => {
 equipObject("Programming language","JavaScript", "full");
 equipObject("Superset","TypeScript", "full");
 displayObject();
+
+class VirtualMachine {
+
+    private isActivated: boolean = false
+
+    constructor(
+        public readonly computerName: string
+    ) {}
+
+    on(): boolean{
+        try {
+            this.isActivated = true;
+            console.log("\n     Loading the system in progress....");
+            return this.isActivated
+        }catch (err){
+            throw new Error("     An unexpected error has occurred. The computer can not be started at this time")
+        }
+    }
+
+}
+
+const pc = new VirtualMachine("PLWAWWSW2000EXTERNAL");
+if(pc.on()){
+    setTimeout(() => {
+        console.log("\n     The Virtual Machine is ready");
+    }, 4000);
+}
+
+
+
+
+class MainAlgorithm {
+    static readonly repository: object[] = [];
+    constructor(
+        protected computeSimulation: string
+    ){}
+
+    inUse(simulation: object){
+        MainAlgorithm.repository.push(simulation);
+    }
+
+}
+
+class Dl extends MainAlgorithm {
+
+    constructor(computeSimulation: string) {
+        super(computeSimulation);
+    }
+
+}
+
+class Ml extends MainAlgorithm {
+
+    constructor(computeSimulation: string) {
+        super(computeSimulation);
+    }
+
+}
+
+const mainAl = new MainAlgorithm("DEV-7890-EPP");
+
+mainAl.inUse(new Dl("DEXTER_SearchAnalyzer"));
+mainAl.inUse(new Ml("CODE_SNIPPET_Generator"));
+
+console.log(MainAlgorithm.repository);
